ed1919b23148c70527bf3f5c95ce29c9e9f17148
nestakiev <n3st1k13@gmail.com>
2022-06-20 21:54:23 +0300

    complite task-3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,49 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const refs = {
+  form: document.querySelector('.form'),
+}
+
+refs.form.addEventListener('submit', onSubmitForm);
+
+function onSubmitForm(event) {
+  event.preventDefault();
+  const {
+    elements: { delay, step, amount }
+  } = event.currentTarget;
+
+  let totalDelay = 0;
+  let position = 0;
+
+  for (let i = 0; i < amount.value; i += 1) {
+    position += 1;
+    totalDelay = Number(delay.value) + Number(step.value) * Number(i);
+        
+    PromiseResult(position, totalDelay);
   }
 }
+
+function createPromise(position, delay) {
+  const shouldResolve = Math.random() > 0.3;
+  return new Promise((resolve, reject) =>
+  setTimeout(() => {  
+  if (shouldResolve) {
+    resolve(({position, delay}));
+  } else {
+    reject(({position, delay}));
+  }  
+}, delay));
+}
+
+function PromiseResult (position, totalDelay) {
+  createPromise(position, totalDelay).then(({ position, delay}) => {
+    Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay}) => {
+    Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+  })
+}
+
+
+
+
